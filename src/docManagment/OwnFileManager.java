2e8bf05005d95fb7a98dbe414197a0adabe6a812
Melania Morales <melaniacrc@gmail.com>
2021-10-13 13:24:40 -0600

    U: readDoc function

--- src/docManagment/OwnFileManager.java
+++ src/docManagment/OwnFileManager.java
@@ -10,7 +10,7 @@ import java.util.List;
 public class OwnFileManager {
     public static void main(String[] args) throws Exception {
 
-        String fileName = "C:\\Users\\melan\\OneDrive\\6. TEC-SEXTO SEMESTRE\\RECUPERACION DE INFORMACION TEXTUAL\\PROYECTO 2\\Colecciones\\h8.txt";
+        String fileName = "C:\\Users\\melan\\OneDrive\\6. TEC-SEXTO SEMESTRE\\RECUPERACION DE INFORMACION TEXTUAL\\PROYECTO 2\\Colecciones\\prueba.txt";
 
         long before = System.nanoTime();
         List<String> result = readBuffered(fileName);
@@ -27,22 +27,19 @@ public class OwnFileManager {
         BufferedReader brRafReader = new BufferedReader(
                 new FileReader(randomAccessFile.getFD()));
         String line = null;
-        long currentOffset = 0;
-        long previousOffset = -1;
+        long currentPosition = -1;
+        long previousOffset = 0;
+        int bufferOffset = 0;
         while ((line = brRafReader.readLine()) != null) {
             long fileOffset = randomAccessFile.getFilePointer();
-            if (fileOffset != previousOffset) {
-                if (previousOffset != -1) {
-                    currentOffset = previousOffset;
-                }
-                previousOffset = fileOffset;
-            }
-            int bufferOffset = getOffset(brRafReader);
-            long realPosition = currentOffset + bufferOffset;
-            System.out.println("Position : " + realPosition 
-                    + " with FP " + randomAccessFile.getFilePointer()
+            previousOffset = bufferOffset;
+            bufferOffset = getOffset(brRafReader);
+            long realPosition = bufferOffset;
+            currentPosition += previousOffset+1; //Sumo 1 del salto de linea
+            System.out.println("Initial position : " + currentPosition 
                     + " and offset " + bufferOffset);
             lines.add(line);
+        	currentPosition = 0;
         }
         return lines;
     }
@@ -57,16 +54,5 @@ public class OwnFileManager {
             field.setAccessible(false);
         }
         return result;
-    }
-
-    private static List<String> readDefault(String fileName) throws Exception {
-        List<String> lines = new ArrayList<String>();
-        RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "r");
-        String line = null;
-        while ((line = randomAccessFile.readLine()) != null) {
-            System.out.println("Position : " + randomAccessFile.getFilePointer());
-            lines.add(line);
-        }
-        return lines;
     }
 }
