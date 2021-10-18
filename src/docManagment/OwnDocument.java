1adaab0d5f3642c20c2a34c617d4879db70bbc8f
Melania Morales <melaniacrc@gmail.com>
2021-10-17 18:48:41 -0600

    U: readColection function

--- src/docManagment/OwnDocument.java
+++ src/docManagment/OwnDocument.java
@@ -1,42 +1,50 @@
 package docManagment;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.FileReader;
-import java.io.InputStreamReader;
 import java.io.RandomAccessFile;
 import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.List;
 
-public abstract class OwnFileManager {
+public class OwnFileManager {
+    public static void main(String[] args) throws Exception {
 
-    public static List<String> readColection(String fileName) throws Exception {
-    	DocProcessing documentProcessing = new DocProcessing();
+        String fileName = "C:\\Users\\melan\\OneDrive\\6. TEC-SEXTO SEMESTRE\\RECUPERACION DE INFORMACION TEXTUAL\\PROYECTO 2\\Colecciones\\prueba.txt";
+
+        long before = System.nanoTime();
+        List<String> result = readBuffered(fileName);
+        //List<String> result = readDefault(fileName);
+        long after = System.nanoTime();
+        double ms = (after - before) / 1e6;
+        System.out.println("Reading took " + ms + "ms "
+                + "for " + result.size() + " lines");
+    }
+
+    private static List<String> readBuffered(String fileName) throws Exception {
         List<String> lines = new ArrayList<String>();
         RandomAccessFile randomAccessFile = new RandomAccessFile(fileName, "r");
-        //BufferedReader brRafReader = new BufferedReader(new FileReader(randomAccessFile.getFD()));
-        BufferedReader brRafReader = new BufferedReader(new InputStreamReader(new FileInputStream(randomAccessFile.getFD()), "ISO-8859-1"));
-        RandomAccessFile raf = new RandomAccessFile(fileName, "r");
+        BufferedReader brRafReader = new BufferedReader(
+                new FileReader(randomAccessFile.getFD()));
         String line = null;
         long currentOffset = 0;
         long previousPosition = 0;
-        long previousOffset = -1;
+        long previousOffsetFlag = -1;
         long initialPosition = -1;
         int bufferOffset = 0;
         long actualPosition = 0;
         String doc = "";
-        long docID=1;
+        int docID=1;
         while ((line = brRafReader.readLine()) != null) {
-        	long fileOffset = randomAccessFile.getFilePointer();
-            if (fileOffset != previousOffset) {
-                if (previousOffset != -1) {
-                    currentOffset = previousOffset;
+            long fileOffset = randomAccessFile.getFilePointer();
+            if (fileOffset != previousOffsetFlag) {
+                if (previousOffsetFlag != -1) {
+                    currentOffset = previousOffsetFlag;
                 }
-                previousOffset = fileOffset;
+                previousOffsetFlag = fileOffset;
             }
             
-            if(line.matches("<!DOCTYPE html PUBLIC \"-//W3C//DTD XHTML 1.0 Transitional//EN\" \"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd\">.*")) {
+            if(line.matches("<!DOCTYPE .*")) {
             	System.out.println("DOC ID: " + docID);
                 bufferOffset = getOffset(brRafReader);
                 actualPosition=currentOffset+bufferOffset;
@@ -49,14 +57,12 @@ public abstract class OwnFileManager {
                 System.out.println("Initial position : " + initialPosition 
                         + " and offset " + actualPosition);
                 
-                OwnDocument actualDoc = new OwnDocument(docID, (actualPosition-initialPosition), initialPosition);
-                documentProcessing.getDocuments().add(actualDoc);
-                
-                /*raf.seek(initialPosition);
+                RandomAccessFile raf = new RandomAccessFile("C:\\Users\\melan\\OneDrive\\6. TEC-SEXTO SEMESTRE\\RECUPERACION DE INFORMACION TEXTUAL\\PROYECTO 2\\Colecciones\\prueba.txt", "rw");
+                raf.seek(initialPosition);
                 byte[] arr = new byte[(int) (actualPosition-initialPosition)];
                 raf.readFully(arr);
                 String text = new String(arr);
-                System.out.println(text);*/
+                System.out.println(text);
                 
                 doc.concat(line);
                 initialPosition=0;
@@ -69,9 +75,6 @@ public abstract class OwnFileManager {
             }
           previousPosition=actualPosition;
         }
-        randomAccessFile.close();
-        raf.close();
-        documentProcessing.processTagsInDoc();
         return lines;
     }
 
